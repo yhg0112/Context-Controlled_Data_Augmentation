6c25f3a82a33bfa4c7605e405956a28304d78e6b
yhg <youaredead@snu.ac.kr>
2022-04-27 14:10:41 +0900

    initial structures

---
+++ data_utils.py
@@ -0,0 +1,12 @@
+
+
+# misc functions
+
+
+# functions for subsampling
+## save to temp json or csv
+
+
+# functions for masking
+
+
---
+++ main.sh
@@ -0,0 +1,16 @@
+# load config 
+
+
+# fine-tune Text Classification model with sub-sampled dataset (GPU: TC)
+
+
+# mask dataset if masked_datset file does not exist (GPU: TC)
+
+
+# fine-tune Text Generation model with the masked dataset (GPU: TG)
+
+
+# generate dataset if generated_dataset file does not exist (GPU: TG)
+
+
+# fine-tune TC model again (GPU: TC)
