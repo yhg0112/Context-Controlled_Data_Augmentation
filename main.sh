f2c306d495e3569a212193873bb74497b19b4a30
Minwoo Lee <minwoolee@snu.ac.kr>
2022-05-11 09:40:14 +0900

    Split train val and save best run

--- main.sh
+++ main.sh
@@ -33,9 +33,10 @@ fi
 
 # mask dataset if masked_datset file does not exist (GPU: TC Inference)
 if [ "$1" = "mask_dataset" ]; then
-  MODEL_NAME="./models/subsample-roberta-base-3029/checkpoint-3000"
+  # MODEL_NAME="./models/subsample-roberta-base-3029/checkpoint-3000"
+  MODEL_NAME="./models/classification/roberta-3536/checkpoint-303"
   IN_DATA="./data/yelp_subsample=0.01_seed=42_train.jsonl"
-  MASK_P=0.2
+  MASK_P="$2"
   python mask_dataset.py \
     --model_name_or_path "$MODEL_NAME" \
     --in_data "$IN_DATA" \
@@ -48,24 +49,27 @@ fi
 if [ "$1" = "train_generation" ]; then
   MODEL_NAME="google/t5-v1_1-small"
   MODEL_OUT_DIR="./models/generation/t5-${OUT_ID}"
+  MASK_P="$2"
   python train_generation.py \
       --model_name_or_path "$MODEL_NAME" \
-      --in_data "./data/masked/yelp_subsample=0.01_seed=42_mask_p=0.7_masked_train.jsonl" \
+      --in_data "./data/masked/yelp_subsample=0.01_seed=42_mask_p=${MASK_P}_masked_train.jsonl" \
       --output_dir "$MODEL_OUT_DIR" \
-      --evaluation_strategy steps \
-      --eval_steps 500 \
-      --learning_rate 3e-4 \
-      --warmup_steps 50 \
-      --per_device_train_batch_size 16 \
-      --per_device_eval_batch_size 64 \
-      --gradient_accumulation_steps 4 \
-      --weight_decay 0.01 \
-      --save_total_limit 15 \
-      --num_train_epochs 50 \
-      --predict_with_generate \
       --bf16 \
-      --metric_for_best_model bleu \
-      --seed 42
+      --seed 42 \
+      --learning_rate 3e-4 \
+      --weight_decay 0.01 \
+      --per_device_train_batch_size 32 \
+      --gradient_accumulation_steps 2 \
+      --per_device_eval_batch_size 64 \
+      --evaluation_strategy steps \
+      --warmup_steps 50 \
+      --num_train_epochs 100 \
+      --eval_every_n_epochs 1 \
+      --early_stopping_patience 10 \
+      --predict_with_generate \
+      --load_best_model_at_end \
+      --metric_for_best_model 'bleu' \
+      --save_total_limit 1
 fi
 
 
--- train_generation.py
+++ train_generation.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 from datasets import load_dataset, load_metric
-from transformers import AutoModelForSeq2SeqLM, AutoTokenizer, HfArgumentParser, DataCollatorForSeq2Seq, \
+from transformers import AutoModelForSeq2SeqLM, AutoTokenizer, EarlyStoppingCallback, HfArgumentParser, DataCollatorForSeq2Seq, \
     Seq2SeqTrainingArguments, Seq2SeqTrainer
 
 
@@ -45,7 +45,6 @@ def postprocess_text(preds, labels):
 
     return preds, labels
 
-
 def main(args, training_args):
     gen_model = AutoModelForSeq2SeqLM.from_pretrained(args.model_name_or_path, cache_dir=MODEL_CACHE_DIR)
     gen_tokenizer = AutoTokenizer.from_pretrained(args.model_name_or_path, cache_dir=MODEL_CACHE_DIR) # ==> SentencePiece
@@ -55,10 +54,10 @@ def main(args, training_args):
     # add roberta's mask_token to the t5 tokenizer and increase the embedding size.
     gen_model.resize_token_embeddings(len(gen_tokenizer))
 
-    yelp_dataset = load_dataset('json', data_files=args.in_data, cache_dir=DATA_CACHE_DIR)
+    yelp_dataset = load_dataset('json', data_files=args.in_data, cache_dir=DATA_CACHE_DIR, split='train')
+    yelp_dataset = yelp_dataset.train_test_split(test_size=0.1, seed=training_args.seed)
     tokenized_yelp_dataset = load_tokenized_dataset(yelp_dataset, tokenizer=gen_tokenizer, max_len=512)
 
-    data_collator = DataCollatorForSeq2Seq(gen_tokenizer, padding=True)
 
     metric = load_metric("sacrebleu", DATA_CACHE_DIR)
 
@@ -83,16 +82,26 @@ def main(args, training_args):
         result = {k: round(v, 4) for k, v in result.items()}
         return result
 
+    # Convert eval_every_n_epochs to eval_steps
+    train_data_size = len(tokenized_yelp_dataset['train'])
+    print("Train dataset size: ", train_data_size)
+    total_train_batch_size = training_args.train_batch_size * training_args.gradient_accumulation_steps * training_args.world_size
+    print("Total Train batch size: ", total_train_batch_size)
+    training_args.eval_steps = int(train_data_size / total_train_batch_size * args.eval_every_n_epochs)
+    training_args.save_steps = training_args.eval_steps
+    print("Eval every {} epochs = every {} steps".format(args.eval_every_n_epochs, training_args.eval_steps))
 
     trainer = Seq2SeqTrainer(
         gen_model,
         training_args,
         train_dataset=tokenized_yelp_dataset["train"].remove_columns("label"),
-        eval_dataset=tokenized_yelp_dataset["train"].remove_columns("label"),  # TODO what split to use for eval?
-        data_collator=data_collator,
+        eval_dataset=tokenized_yelp_dataset["test"].remove_columns("label"),
         tokenizer=gen_tokenizer,
-        compute_metrics=compute_metrics
+        data_collator=DataCollatorForSeq2Seq(gen_tokenizer, padding=True),
+        compute_metrics=compute_metrics,
+        callbacks=[EarlyStoppingCallback(early_stopping_patience=args.early_stopping_patience)],
     )
+
     trainer.train()
 
 
@@ -100,5 +109,7 @@ if __name__ == "__main__":
     parser = HfArgumentParser((Seq2SeqTrainingArguments,))
     parser.add_argument('--in_data', type=str, required=True)
     parser.add_argument('--model_name_or_path', type=str, required=True)
+    parser.add_argument('--eval_every_n_epochs', type=float, default=0.5)
+    parser.add_argument('--early_stopping_patience', type=int, default=10)
     training_args, additional_args = parser.parse_args_into_dataclasses()
     main(additional_args, training_args)
